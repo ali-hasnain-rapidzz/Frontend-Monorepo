bf4bae37c60f94584bfe498ace5a175793b9e53b
sokainakanwal1 <sokainakanwal@rapidzzsolutions.com>
2024-08-05 15:13:53 +0500

    initial commint

---
+++ packages/common/src/Services/http.service.ts
@@ -0,0 +1,53 @@
+import fetch, { HeadersInit, RequestInit, BodyInit } from "node-fetch";
+
+type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
+
+interface HttpRequestOptions {
+  body?: any;
+  headers?: HeadersInit;
+}
+
+class HttpServiceClass {
+  private async request(endpoint: string, method: HttpMethod, options?: HttpRequestOptions) {
+    const url = endpoint;
+    const headers: HeadersInit = {
+      "Content-Type": "application/json",
+      ...options?.headers,
+    };
+
+    const config: RequestInit = {
+      method,
+      headers,
+      body: options?.body ? JSON.stringify(options.body) as BodyInit : undefined,
+    };
+
+    try {
+      const response = await fetch(url, config);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("HTTP Request Failed: ", error);
+      throw error;
+    }
+  }
+
+  public get(endpoint: string, options?: HttpRequestOptions) {
+    return this.request(endpoint, "GET", options);
+  }
+
+  public post(endpoint: string, body: any, options?: HttpRequestOptions) {
+    return this.request(endpoint, "POST", { ...options, body });
+  }
+
+  public put(endpoint: string, body: any, options?: HttpRequestOptions) {
+    return this.request(endpoint, "PUT", { ...options, body });
+  }
+
+  public delete(endpoint: string, options?: HttpRequestOptions) {
+    return this.request(endpoint, "DELETE", options);
+  }
+}
+
+export const HttpService = new HttpServiceClass();
