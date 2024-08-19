b65f94390f145294fa1658cb8f8ee9bd38100235
sokainakanwal1 <sokainakanwal@rapidzzsolutions.com>
2024-08-19 13:00:26 +0500

    .

--- apps/user/src/app/components/home.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const WelcomeMessage = () => (
-  <div className="welcome">
-    <h1 className="welcome__text">Welcome to User Side</h1>
-  </div>
-);
-
-export default WelcomeMessage;
